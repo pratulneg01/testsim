508cc527296739c4cc159810eecf472b3a7b9ccd
Pratul Negredo <pratulneg@gmail.com>
2023-11-15 20:40:35 +0100

    Code with error

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -3,6 +3,8 @@
 mkdir -p res/genome
 wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
+gunzip res/genome/ecoli.fasta.gz
+
 echo "Running STAR index..."
 mkdir -p res/genome/star_index
 STAR --runThreadN 4 --runMode genomeGenerate \
@@ -10,7 +12,8 @@ STAR --runThreadN 4 --runMode genomeGenerate \
 --genomeFastaFiles res/genome/ecoli.fasta \
 --genomeSAindexNbases 9
 
-for $sampleid in $()
+for $sampleid in $(ls data/*.fastq.gz | cut -d"_" -f1 | sed "s:data/::" | sort | uniq)
 do
-    bash analyse_script.sh $sampleid
+    bash analyse_sample.sh data/${sampleid}_1
+    bash analyse_sample.sh data/${sampleid}_2
 done
