b47ff7bdc7acf433aaf50e5dfa7049afe2c869ce
Pratul Negredo <pratulneg@gmail.com>
2023-11-15 20:25:22 +0100

    Added STAR indexing

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -3,3 +3,9 @@
 mkdir -p res/genome
 wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
+echo "Running STAR index..."
+mkdir -p res/genome/star_index
+STAR --runThreadN 4 --runMode genomeGenerate \
+--genomeDir res/genome/star_index/ \
+--genomeFastaFiles res/genome/ecoli.fasta \
+--genomeSAindexNbases 9
